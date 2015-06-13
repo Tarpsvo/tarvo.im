ce988baf7da92f9707cbc517dd7e59d02bdb43f2
Tarvo R <tarvo@tramvai.im>
2015-06-13 21:30:18 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,25 @@
+## Tramvai.im
+### Personal website
+Live version is accessible at [tramvai.im](http://tramvai.im/)
+
+### Current functionalities
+* N/A
+
+### TODO list
+* Endomondo data querying
+* GitHub data querying
+* Notes system (blog)
+* jQuery animations
+* Data stored locally and updated by a cron job
+
+### Used resources
+* [Endomondo logo](https://www.iconfinder.com/icons/143904/edomondo_icon)
+* [Github logo](https://github.com/logos)
+
+### Licensed under the GNU GPL v2.0 license
+Copyright (C) 2015 T. Reinpalu
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Tramvai.im</title>
+        <link rel="stylesheet" href="css/reset.css" charset="utf-8">
+        <link rel="stylesheet" href="css/main.css" charset="utf-8">
+        <link rel="stylesheet" href="css/boxes.css" charset="utf-8">
+    </head>
+
+    <body>
+        <div class="absolute-center" id="page-content-wrap">
+            <div class="left-side-box" style="top: 0;">
+                <h1 class="box-title">Sport</h1>
+
+                <div class="box-content left-side-box-content">
+                    <div class="large-stats absolute-center">
+                        <h2>423 workouts</h2>
+                        <h2>6923 kilometres</h2>
+                    </div>
+
+                    <div class="tracker-logo" id="tracker-endomondo"></div>
+                </div>
+            </div>
+
+            <div class="left-side-box" style="bottom: 0;">
+                <h1 class="box-title">Coding</h1>
+
+                <div class="box-content left-side-box-content">
+                    <div class="left-side-box-content">
+                        <div class="large-stats absolute-center">
+                            <h2>6 projects</h2>
+                            <h2>123 commits</h2>
+                        </div>
+
+                        <div class="tracker-logo" id="tracker-github"></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="notes-box">
+                <h1 class="box-title">Notes</h1>
+
+                <div class="box-content notes-box-content">
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
