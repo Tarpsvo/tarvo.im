a4c8b74bc5b6889d8e51544d2d9f3b041c4fa5c4
Tarvo R <tarvo@tramvai.im>
2015-06-14 19:17:41 +0300

    Style fixes, modals now closed through button click.

--- js/tramvaiApp/viewController.js
+++ js/tramvaiApp/viewController.js
@@ -6,26 +6,45 @@
         .controller('ViewController', ViewController);
 
     function ViewController($scope, $rootScope) {
+        $scope.modalOpen = false;
+
         /* Morphs the box into full size on click */
         $scope.openModal = function(event) {
             var div = $(event.currentTarget);
             var marginTop = (div.parent().is('#coding-box')) ? '-370px' : '-30px';
 
-            if (div.hasClass('modal-opened')) {
-                div.removeClass('modal-opened');
-                div.css('margin-top', '0px');
+            div.addClass('modal-opened');
+            div.removeClass('box-content');
+            div.removeClass('left-side-box-content');
+            div.removeClass('modal-trigger');
+            div.find('.close-modal-button').fadeIn(500);
 
-                if (div.parent().is('#notes-box')) {
-                    div.css('margin-left', '0px');
-                }
-            } else {
-                div.addClass('modal-opened');
-                div.css('margin-top', marginTop);
+            div.css('margin-top', marginTop);
 
-                if (div.parent().is('#notes-box')) {
-                    div.css('margin-left', '-360px');
-                }
+            if (div.parent().is('#notes-box')) {
+                div.css('margin-left', '-360px');
             }
+
+            $scope.modalOpen = true;
+        };
+
+        /* Morphs the box back to a small box */
+        $scope.closeModal = function(event) {
+            var div = $(event.currentTarget).parent();
+
+            div.removeClass('modal-opened');
+            div.addClass('box-content');
+            div.addClass('left-side-box-content');
+            div.addClass('modal-trigger');
+            div.find('.close-modal-button').hide();
+
+            div.css('margin-top', '0px');
+
+            if (div.parent().is('#notes-box')) {
+                div.css('margin-left', '0px');
+            }
+
+            $scope.modalOpen = false;
         };
     }
 })();
