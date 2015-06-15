c4e42eeb0f9339e40a57e8d35ac239863cc457bf
Tarvo R <tarvo@tramvai.im>
2015-06-15 20:54:44 +0300

    Fixed bug in ripple.

--- js/ui/ripple.js
+++ js/ui/ripple.js
@@ -1,29 +1,32 @@
 var ink, d, x, y;
 $(".ripple").click(function(e) {
-    if ($(this).find(".ink").length === 0) {
-        $(this).prepend("<span class='ink'></span>");
+    var currentDiv = $(this);
+    var parentDiv = currentDiv.parent();
+
+    if (currentDiv.find(".ink").length === 0) {
+        currentDiv.prepend("<span class='ink'></span>");
     }
 
-    ink = $(this).find(".ink");
+    ink = currentDiv.find(".ink");
     ink.removeClass("animate");
 
-    if(!ink.height() && !ink.width()){
-        d = Math.max($(this).outerWidth(), $(this).outerHeight());
+    if (!ink.height() && !ink.width()) {
+        d = currentDiv.outerWidth();
         ink.css({height: d, width: d});
     }
 
     /* Coding box uses 'bottom' for positioning, Notes box uses 'right' */
-    if ($(this).parent().is('#coding-box')) {
-        x = e.pageX - $(this).offset().left - ink.width()/2;
-        y = Math.abs(e.pageY - $(this).offset().top - $(this).height()) - ink.height()/2;
+    if (parentDiv.is('#coding-box')) {
+        x = e.pageX - currentDiv.offset().left - ink.width()/2;
+        y = Math.abs(e.pageY - currentDiv.offset().top - currentDiv.height()) - ink.height()/2;
         ink.css({bottom: y, left: x}).addClass("animate");
-    } else if ($(this).parent().is('#notes-box')) {
-        x = Math.abs(e.pageX - $(this).offset().left - $(this).width()) - ink.height()/2;
-        y = e.pageY - $(this).offset().top - ink.height()/2;
+    } else if (parentDiv.is('#notes-box')) {
+        x = Math.abs(e.pageX - currentDiv.offset().left - currentDiv.width()) - ink.height()/2;
+        y = e.pageY - currentDiv.offset().top - ink.height()/2;
         ink.css({top: y+'px', right: x+'px'}).addClass("animate");
     } else {
-        x = e.pageX - $(this).offset().left - ink.width()/2;
-        y = e.pageY - $(this).offset().top - ink.height()/2;
+        x = e.pageX - currentDiv.offset().left - ink.width()/2;
+        y = e.pageY - currentDiv.offset().top - ink.height()/2;
         ink.css({top: y+'px', left: x+'px'}).addClass("animate");
     }
 });
