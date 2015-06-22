a2ac1bcb4385a4de6a0034fa755fcfe7095d29bf
Tarvo R <tarvo@tramvai.im>
2015-06-22 21:02:28 +0300

    Reworked update (previously install) script.

--- api/dataUpdateApi.php
+++ api/dataUpdateApi.php
@@ -12,58 +12,69 @@ class dataUpdateApi {
         $this->connection = mainConfig::connectToDatabase();
     }
 
-    public function updateGithubGeneralStats() {
+    public function updateGithubStats() {
         $reposList = $this->githubApi->getListOfRepos();
 
-        foreach ($reposList as $key => $repo) {
-            $commits = $this->githubApi->getNumberOfCommitsFromRepoUrl($repo->url);
+        if (!empty($reposList)) {
+            foreach ($reposList as $key => $repo) {
+                $commits = $this->githubApi->getNumberOfCommitsFromRepoUrl($repo->url);
 
-            $unpreparedSQL = "REPLACE INTO github_repos (id, name, created, pushed, commits, html_url)
-                                VALUES (:id, :name, :created, :pushed, :commits, :html_url)";
+                $unpreparedSQL = "REPLACE INTO github_repos (id, name, created, pushed, commits, html_url)
+                                    VALUES (:id, :name, :created, :pushed, :commits, :html_url)";
 
-            $query = $this->connection->prepare($unpreparedSQL);
-            $query->bindParam(':id', $repo->id);
-            $query->bindParam(':name', $repo->name);
-            $query->bindParam(':created', $repo->created_at);
-            $query->bindParam(':pushed', $repo->pushed_at);
-            $query->bindParam(':commits', $commits);
-            $query->bindParam(':html_url', $repo->html_url);
-            $query->execute();
+                $query = $this->connection->prepare($unpreparedSQL);
+                $query->bindParam(':id', $repo->id);
+                $query->bindParam(':name', $repo->name);
+                $query->bindParam(':created', $repo->created_at);
+                $query->bindParam(':pushed', $repo->pushed_at);
+                $query->bindParam(':commits', $commits);
+                $query->bindParam(':html_url', $repo->html_url);
+                $query->execute();
+            }
+            return true;
+        } else {
+            return false;
         }
     }
 
     public function updateEndomondoWorkouts() {
         $allWorkouts = $this->endomondoApi->getAllWorkouts();
 
-        foreach ($allWorkouts as $key => $workout) {
-            if (isset($workout)) {
-                $unpreparedSQL = "INSERT IGNORE INTO endomondo_workouts (sport, distance, duration, speed_avg, calories, start_time, altitude_min, altitude_max, descent, ascent, privacy_workout, workout_id, hydration, peptalks, likes, comments, burgers_burned, device_workout_id, owner_id, privacy_map, speed_max)
-                                    VALUES (:sport, :distance, :duration, :speed_avg, :calories, :start_time, :altitude_min, :altitude_max, :descent, :ascent, :privacy_workout, :workout_id, :hydration, :peptalks, :likes, :comments, :burgers_burned, :device_workout_id, :owner_id, :privacy_map, :speed_max)";
+        if (!empty($allWorkouts)) {
+            foreach ($allWorkouts as $key => $workout) {
+                if (isset($workout)) {
+                    $unpreparedSQL = "INSERT IGNORE INTO endomondo_workouts (sport, distance, duration, speed_avg, calories, start_time, altitude_min, altitude_max, descent, ascent, privacy_workout, workout_id, hydration, peptalks, likes, comments, burgers_burned, device_workout_id, owner_id, privacy_map, speed_max)
+                                        VALUES (:sport, :distance, :duration, :speed_avg, :calories, :start_time, :altitude_min, :altitude_max, :descent, :ascent, :privacy_workout, :workout_id, :hydration, :peptalks, :likes, :comments, :burgers_burned, :device_workout_id, :owner_id, :privacy_map, :speed_max)";
 
-                $query = $this->connection->prepare($unpreparedSQL);
-                $query->bindParam(':sport', $workout->sport);
-                $query->bindParam(':distance', $workout->distance);
-                $query->bindParam(':duration', $workout->duration);
-                $query->bindParam(':speed_avg', $workout->speed_avg);
-                $query->bindParam(':calories', $workout->calories);
-                $query->bindParam(':start_time', $workout->start_time);
-                $query->bindParam(':altitude_min', $workout->altitude_min);
-                $query->bindParam(':altitude_max', $workout->altitude_max);
-                $query->bindParam(':descent', $workout->descent);
-                $query->bindParam(':ascent', $workout->ascent);
-                $query->bindParam(':privacy_workout', $workout->privacy_workout);
-                $query->bindParam(':workout_id', $workout->id);
-                $query->bindParam(':hydration', $workout->hydration);
-                $query->bindParam(':peptalks', $workout->lcp_count->peptalks);
-                $query->bindParam(':likes', $workout->lcp_count->likes);
-                $query->bindParam(':comments', $workout->lcp_count->comments);
-                $query->bindParam(':burgers_burned', $workout->burgers_burned);
-                $query->bindParam(':device_workout_id', $workout->device->workout_id);
-                $query->bindParam(':owner_id', $workout->owner_id);
-                $query->bindParam(':privacy_map', $workout->privacy_map);
-                $query->bindParam(':speed_max', $workout->speed_max);
-                $query->execute();
+                    $query = $this->connection->prepare($unpreparedSQL);
+                    $query->bindParam(':sport', $workout->sport);
+                    $query->bindParam(':distance', $workout->distance);
+                    $query->bindParam(':duration', $workout->duration);
+                    $query->bindParam(':speed_avg', $workout->speed_avg);
+                    $query->bindParam(':calories', $workout->calories);
+                    $query->bindParam(':start_time', $workout->start_time);
+                    $query->bindParam(':altitude_min', $workout->altitude_min);
+                    $query->bindParam(':altitude_max', $workout->altitude_max);
+                    $query->bindParam(':descent', $workout->descent);
+                    $query->bindParam(':ascent', $workout->ascent);
+                    $query->bindParam(':privacy_workout', $workout->privacy_workout);
+                    $query->bindParam(':workout_id', $workout->id);
+                    $query->bindParam(':hydration', $workout->hydration);
+                    $query->bindParam(':peptalks', $workout->lcp_count->peptalks);
+                    $query->bindParam(':likes', $workout->lcp_count->likes);
+                    $query->bindParam(':comments', $workout->lcp_count->comments);
+                    $query->bindParam(':burgers_burned', $workout->burgers_burned);
+                    $query->bindParam(':device_workout_id', $workout->device->workout_id);
+                    $query->bindParam(':owner_id', $workout->owner_id);
+                    $query->bindParam(':privacy_map', $workout->privacy_map);
+                    $query->bindParam(':speed_max', $workout->speed_max);
+                    $query->execute();
+                }
             }
+            return true;
+        } else {
+            return false;
         }
+
     }
 }
